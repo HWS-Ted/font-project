0fe1099520f57c3d06f1a8fe8e6c8eca93470b4d
HWS-Ted <f772886@gmail.com>
2023-12-05 13:20:41 +0800

    HTML CSS setting 1.0

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuButton = document.getElementById("menu-button");
-    var menu = document.getElementById("menu");
-  
-    menuButton.addEventListener("click", function () {
-      if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex"; // 点击按钮时显示菜单，并应用 Flexbox 布局
-      } else {
-        menu.style.display = "none"; // 再次点击按钮时隐藏菜单
-      }
-    })
-  
-    document.addEventListener("click", function (event) {
-      if (!menu.contains(event.target) && event.target !== menuButton) {
-        menu.style.display = "none";
-      }
-    });
+  const menu = document.getElementById("menu");
+  const menuArrow = document.querySelector("#menu-arrow");
+
+  menuArrow.addEventListener("click", function () {
+    if (menu.style.display === "none" || menu.style.display === "") {
+      menu.style.display = "flex"; // 點擊時顯示，並使用 flexbox
+      menuArrow.className = "fa-solid fa-circle-chevron-up fa-lg";
+    } else {
+      menu.style.display = "none"; // 再次點擊時隱藏
+      menuArrow.className = "fa-solid fa-circle-chevron-down fa-lg";
+    }
   });
+
+  document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && event.target !== menuArrow) {
+      menu.style.display = "none";
+    }
+  });
+});
